51c12771ed3b8e2f002c4559781163f00c525e08
ForestTracker <39146448+mathematicie@users.noreply.github.com>
2021-12-29 19:50:46 +0530

    Create About.java

---
+++ java/About.java
@@ -0,0 +1,17 @@
+//Package name ---
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        if (getSupportActionBar()!=null)
+            getSupportActionBar().hide();
+    }
+}
